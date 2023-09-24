320b0a766e2c86d93a0eec6855e7ac2958862884
MachaDevInc <125951486+MachaDevInc@users.noreply.github.com>
2023-09-25 03:12:33 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -74,49 +74,59 @@ if [ "$output" == 2 ]; then
 
     mount /dev/mmcblk0p2 /overlay
 
+    sed -i "s+option[[:space:]]\+enabled[[:space:]]\+'0'+option enabled '1'+g" /etc/config/fstab
+    sed -i "s+'/mnt/mmcblk0p2'+'/overlay'+g" /etc/config/fstab
+
+    cd
+
+    git clone https://github.com/MachaDevInc/VM_OnionOmega2S.git
+
+    cp /root/VM_OnionOmega2S/* /root/
+    rm -r /root/VM_OnionOmega2S/
+
     # Update the flag to indicate that step 3 is complete
     echo 3 > /VM/install_step
 
     reboot && exit
 fi
 
-# if [ "$output" == 1 ]; then
-#     echo "Downloading the packages and libraries"
+if [ "$output" == 3 ]; then
+    echo "Downloading the packages and libraries"
 
-#     opkg update
-#     opkg install python3-pip
-#     opkg install python3-setuptools
-#     opkg install python3-cryptography
+    opkg update
+    opkg install python3-pip
+    opkg install python3-setuptools
+    opkg install python3-cryptography
 
-#     pip3 install --upgrade pip
-#     pip3 install stripe
-#     pip3 install python-dotenv
-#     pip3 install pyserial
-#     pip3 install esptool
+    pip3 install --upgrade pip
+    pip3 install stripe
+    pip3 install python-dotenv
+    pip3 install pyserial
+    pip3 install esptool
 
-#     opkg install python3-pyqt5
+    opkg install python3-pyqt5
 
-#     pip3 install flask
-#     pip3 install Flask-Session
-#     pip3 install gunicorn
-#     opkg install nginx
+    pip3 install flask
+    pip3 install Flask-Session
+    pip3 install gunicorn
+    opkg install nginx
 
-#     /etc/init.d/nginx restart
-#     /etc/init.d/my_gunicorn_server enable
+    /etc/init.d/nginx restart
+    /etc/init.d/my_gunicorn_server enable
 
-#     cp /root/omega2s.conf /etc/nginx/conf.d/
-#     rm -r /root/omega2s.conf
+    cp /root/omega2s.conf /etc/nginx/conf.d/
+    rm -r /root/omega2s.conf
 
-#     cp /root/my_gunicorn_server /etc/init.d/
-#     rm -r /root/my_gunicorn_server
-#     chmod +x /etc/init.d/my_gunicorn_server
+    cp /root/my_gunicorn_server /etc/init.d/
+    rm -r /root/my_gunicorn_server
+    chmod +x /etc/init.d/my_gunicorn_server
 
-#     git clone https://github.com/MachaDevInc/esp32s2.git
-#     python3 -m esptool --chip esp32s2 --port /dev/ttyUSB0 --baud 921600  --before default_reset --after hard_reset write_flash --flash_mode dio --flash_size detect --flash_freq 40m 0x1000 /root/esp32s2/VM_ESP32-S2.ino.bootloader.bin 0x8000 /root/esp32s2/VM_ESP32-S2.ino.partitions.bin 0x10000 /root/esp32s2/VM_ESP32-S2.ino.bin
-#     rm -r /root/esp32s2
+    git clone https://github.com/MachaDevInc/esp32s2.git
+    python3 -m esptool --chip esp32s2 --port /dev/ttyUSB0 --baud 921600  --before default_reset --after hard_reset write_flash --flash_mode dio --flash_size detect --flash_freq 40m 0x1000 /root/esp32s2/VM_ESP32-S2.ino.bootloader.bin 0x8000 /root/esp32s2/VM_ESP32-S2.ino.partitions.bin 0x10000 /root/esp32s2/VM_ESP32-S2.ino.bin
+    rm -r /root/esp32s2
 
-#     flag_update=/root/install_step
-#     cat /root/flag_two>$flag_update
+    # Update the flag to indicate that step 3 is complete
+    echo 4 > /VM/install_step
 
-#     reboot && exit
-# fi
+    reboot && exit
+fi
