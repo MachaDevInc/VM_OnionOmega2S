709d830f86f20edccc100b7d01d2b9fd082a1e3f
MachaDevInc <125951486+MachaDevInc@users.noreply.github.com>
2023-11-29 18:33:59 +0500

    Update config_handler.py

--- config_handler.py
+++ config_handler.py
@@ -2,33 +2,33 @@ import json
 
 
 def save_config(data):
-    with open('/root/credentials/config.json', 'w') as outfile:
+    with open('/VM/credentials/config.json', 'w') as outfile:
         json.dump(data, outfile)
 
 
 def load_config():
-    with open('/root/credentials/config.json', 'r') as infile:
+    with open('/VM/credentials/config.json', 'r') as infile:
         data = json.load(infile)
     return data
 
 
 def save_Terminal_config(data):
-    with open('/root/terminal-config/config.json', 'w') as outfile:
+    with open('/VM/terminal-config/config.json', 'w') as outfile:
         json.dump(data, outfile)
 
 
 def load_Terminal_config():
-    with open('/root/terminal-config/config.json', 'r') as infile:
+    with open('/VM/terminal-config/config.json', 'r') as infile:
         data = json.load(infile)
     return data
 
 
 def save_VM_config(data):
-    with open('/root/vm-config/config.json', 'w') as outfile:
+    with open('/VM/vm-config/config.json', 'w') as outfile:
         json.dump(data, outfile)
 
 
 def load_VM_config():
-    with open('/root/vm-config/config.json', 'r') as infile:
+    with open('/VM/vm-config/config.json', 'r') as infile:
         data = json.load(infile)
     return data
