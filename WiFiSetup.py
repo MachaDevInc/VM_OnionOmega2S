a94913d695d59db95f8ea94d268e4e2faed2ac69
MachaDevInc <125951486+MachaDevInc@users.noreply.github.com>
2023-11-29 13:48:22 +0500

    Update WiFiSetup.py

--- WiFiSetup.py
+++ WiFiSetup.py
@@ -11,19 +11,17 @@ ser = serial.Serial(
     timeout=1
 )
 
-while 1:
+Received_Done = False
+
+while not Received_Done:
     ser.write(str.encode('9'))  # transmit data serially
     time.sleep(0.001)
     while ser.in_waiting == 0:
         pass
-    Received = ser.read_until('\n').decode()  # read serial port
-    time.sleep(0.001)
+    Received = ser.read_until('\n').decode().strip()  # read and strip serial port data
 
-    if (Received != ''):
-        Received = Received.replace('\n', '')
-        Received.strip()
-        print(Received)  # print received data
+    print(repr(Received))  # print raw received data for debugging
 
-        if (Received == "okay"):
-            print("Received okay from ESP32-S2")
-            break
+    if Received == "okay":
+        print("Received okay from ESP32-S2")
+        Received_Done = True
