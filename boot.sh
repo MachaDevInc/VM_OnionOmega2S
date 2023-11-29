ff92872ebb2c004d1ecf907774a790c8f246362c
MachaDevInc <125951486+MachaDevInc@users.noreply.github.com>
2023-11-29 18:34:32 +0500

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -11,6 +11,6 @@ else
     echo default-on > /sys/class/leds/omega2p\:amber\:system/trigger
     /etc/init.d/my_gunicorn_server enable >> /VM/gunicorn_output.txt 2>&1
     /etc/init.d/my_gunicorn_server start >> /VM/gunicorn_output.txt 2>&1
-    python3 /VM/WiFiSetup.py >> /VM/output.txt 2>&1
+    # python3 /VM/WiFiSetup.py >> /VM/output.txt 2>&1
     python3 /VM/VM.py >> /VM/output.txt 2>&1
 fi
